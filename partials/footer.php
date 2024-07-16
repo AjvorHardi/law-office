11ba7904c91f0d01178243c33860a72942cbf972
AjvorHardi <markovic.goran@outlook.com>
2024-07-15 21:13:48 -0400

    add footer and separate pages

---
+++ partials/footer.php
@@ -0,0 +1,27 @@
+
+<section class="contact container">
+        <article>
+            <h3>Address</h3>
+            <p>Calgary, Alberta</p>
+            <p>CANADA</p>
+        </article>
+        <article>
+            <h3>Contact</h3>
+            <p>Phone: 3690 777</p>
+            <p>Email: email@lawoffice.com</p>
+        </article>
+        <article>
+            <h3>Business Hours</h3>
+            <p>Monday - Friday 9am - 6pm</p>
+            <p>Saturday 11am - 3pm</p>
+        </article>
+    </section>
+    <section class="map">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d321200.7122810724!2d-114.41747677366483!3d51.02759155103898!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x537170039f843fd5%3A0x266d3bb1b652b63a!2sCalgary%2C%20AB%2C%20Canada!5e0!3m2!1sen!2suk!4v1720837483754!5m2!1sen!2suk" width="100%" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </section>
+    
+    <footer>
+            <p>&copy 2024 GM</p>
+    </footer>
+</body>
+</html>
---
+++ partials/main-header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Law Office</title>
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.css">
+    <link rel="stylesheet" href="scss/style.css">
+</head>
+<body>
+
+    <section class="topBar container">
+        <a href="" class="logo"><span>office</span></a>
+        <ul>
+            <li><a href=""><i class="fab fa-facebook-f"></i></a></li>
+            <li><a href=""><i class="fab fa-twitter"></i></a></li>
+            <li><a href=""><i class="fab fa-instagram"></i></a></li>
+        </ul>
+    </section>
+
+    <header class="main-header">
+        <nav class="container">
+            <ul>
+                <li><a href="index.php">home</a></li>
+                <li><a href="about.php">about</a></li>
+                <li><a href="blog.php">blog</a></li>
+                <li><a href="contact.php">contact</a></li>
+            </ul>
+        </nav>
+        <hgroup>
+            <h2>Experience - Trust - Results</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita illo ratione omnis quisquam quae adipisci magni! Expedita mollitia enim quas dolore adipisci perspiciatis neque.</p>
+            <button>View more</button>
+        </hgroup>
+    </header>
