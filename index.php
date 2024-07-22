c5c42834d62f84a8eb2fa77d9e2948864473e348
AjvorHardi <markovic.goran@outlook.com>
2024-07-21 21:10:08 -0400

    add services section

--- index.php
+++ index.php
@@ -1,5 +1,31 @@
 <?php include 'partials/main-header.php'; ?>
 
-    <h1 style="text-align:center;font-size: 4rem;">Home</h1>
+    <section class="services container">
+        <article>
+            <i class="fas fa-landmark"></i>
+            <h3>Family law</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita facilis qui, eligendi assumenda dolor illo tenetur non tempora fuga ratione fugit quasi natus. Recusandae animi nostrum dolorum ullam deleniti blanditiis nulla?</p>
+            <button>Learn more</button>
+        </article>
+        <article>
+            <i class="fas fa-landmark"></i>
+            <h3>Business law</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita facilis qui, eligendi assumenda dolor illo tenetur non tempora fuga ratione fugit quasi natus. Recusandae animi nostrum dolorum ullam deleniti blanditiis nulla?</p>
+            <button>Learn more</button>
+        </article>
+        <article>
+            <i class="fas fa-landmark"></i>
+            <h3>Criminal law</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita facilis qui, eligendi assumenda dolor illo tenetur non tempora fuga ratione fugit quasi natus. Recusandae animi nostrum dolorum ullam deleniti blanditiis nulla?</p>
+            <button>Learn more</button>
+        </article>
+        <article>
+            <i class="fas fa-landmark"></i>
+            <h3>Insurance law</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita facilis qui, eligendi assumenda dolor illo tenetur non tempora fuga ratione fugit quasi natus. Recusandae animi nostrum dolorum ullam deleniti blanditiis nulla?</p>
+            <button>Learn more</button>
+        </article>
+
+    </section>
 
 <?php include 'partials/footer.php'; ?>
